e52656d664cc05640b1b6c5ca58120b0bdd89679
unsky559 <6kostya@gmail.com>
2021-10-24 15:01:58 +0300

    arc: Hello, World. Add babel dependencies

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+// entry point
+import React from "react";
+import * as ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <h1>Hello, world!</h1>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.body
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js', '.jsx'],
     },
     output: {
         filename: 'bundle.js',
@@ -15,6 +15,15 @@ module.exports = {
             title: 'MathBattle'
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
