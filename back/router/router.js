03ad17e22865ca8e5e7fa2ef46b167f99b8e3b57
IamBogdan <admiral01@mail.ua>
2021-11-24 12:29:07 +0200

    add: sending dist folder to user

--- back/router/router.js
+++ back/router/router.js
@@ -7,7 +7,7 @@ const Router = require('express').Router;
 const router = new Router();
 
 const MIN_USERNAME_LEN = 4;
-const MAX_USERNAME_LEN = 32;
+const MAX_USERNAME_LEN = 16;
 
 const MIN_PASS_LEN = 8;
 const MAX_PASS_LEN = 32;
@@ -35,23 +35,17 @@ function isNotLoggedIn(req, res, next)
   }
 }
 
-router.get('/reg', isNotLoggedIn, (req, res) => {
-  res.send('<h1>/reg</h1>');
-});
-
 router.post('/reg', isNotLoggedIn,
-                    body('username').isLength({ min: MIN_USERNAME_LEN, max: MAX_USERNAME_LEN}),
-                    body('password').isLength({ min: MIN_PASS_LEN, max: MAX_PASS_LEN}),
-                    body('email').isEmail(),
-                    user_controller.register
-                    );
+  body('username').isLength({ min: MIN_USERNAME_LEN, max: MAX_USERNAME_LEN}).withMessage('Invalid username length')
+  .matches(/^[a-zA-Z0-9_-]+$/).withMessage('Invalid characters in username'),
+  body('password').isLength({ min: MIN_PASS_LEN, max: MAX_PASS_LEN}).withMessage('Invalid password length'),
+  body('email').isEmail().withMessage('Invalid email'),
+  user_controller.register
+);
 
 router.post('/login', isNotLoggedIn, user_controller.login);
-router.delete('/logout', isLoggedIn, user_controller.logout);
+router.post('/logout', isLoggedIn, user_controller.logout);
 
-router.get('/login', isNotLoggedIn, (req, res) => {
-  res.send('<h1>/login</h1>');
-});
 
 router.get('/homepage', isLoggedIn, (req, res) => {
   res.send('<h1>Hello! Session created!.</h1>');
