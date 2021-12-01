c3d15658390b1f220100568e4f4532501e0cee01
unsky559 <6kostya@gmail.com>
2021-12-01 09:58:25 +0200

    feat: block game info component

---
+++ client/src/components/blockGameInfo/blockGameInfo.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import SmartIcon from "../smartIcon/smartIcon";
+
+import "../../styles/shared/block.scss";
+import "./blockGameInfo.scss";
+
+const BlockGameInfo = () => {
+    return (
+        <div className="block">
+            <span className="title">Игра</span>
+            <div className="lobbyInfo">
+                <div className="avatar">
+                    <img src="https://thispersondoesnotexist.com/image" alt=""/>
+                </div>
+                <div className="info">
+                    <span className="secondary">Соревновательная</span>
+                    <div className="players">
+                        <a href="#">Playername</a> <span className="secondary">vs</span> <a
+                        href="#">Another</a>
+                    </div>
+                </div>
+            </div>
+            <div className="gameModesRow">
+                <SmartIcon text="2/2" />
+                <SmartIcon text="100" />
+                <SmartIcon text="480" />
+            </div>
+        </div>
+    );
+};
+
+export default BlockGameInfo;
