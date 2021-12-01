9b5fa8782a560a2c7f745e3fc099c43b57763110
unsky559 <6kostya@gmail.com>
2021-12-01 10:05:35 +0200

    feat: block scoreboard component

---
+++ client/src/components/blockScoreboard/blockScoreboard.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import SmartIcon from "../smartIcon/smartIcon";
+
+import "../../styles/shared/block.scss";
+import "./blockScoreboard.scss";
+
+const BlockScoreboard = () => {
+    return (
+        <div className="block">
+            <span className="title">Игроки</span>
+            <div className="scoreboard">
+                <div className="player">
+                    <div className="lobbyInfo">
+                        <div className="avatar">
+                            <img src="https://thispersondoesnotexist.com/image" alt=""/>
+                        </div>
+                        <div className="info">
+                            <a className="playerName" href="#">Playername</a>
+                            <SmartIcon text="225"/>
+                        </div>
+                        <span className="score">50</span>
+                    </div>
+                </div>
+                <div className="player">
+                    <div className="lobbyInfo">
+                        <div className="avatar">
+                            <img src="https://thispersondoesnotexist.com/image" alt=""/>
+                        </div>
+                        <div className="info">
+                            <a className="playerName" href="#">Playername</a>
+                            <SmartIcon text="225"/>
+                        </div>
+                        <span className="score">50</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BlockScoreboard;
