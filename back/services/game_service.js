840fb8847f8231822c785449f367b3712786ac5b
IamBogdan <admiral01@mail.ua>
2021-12-03 23:11:24 +0200

    small changes

--- back/services/game_service.js
+++ back/services/game_service.js
@@ -1,6 +1,9 @@
 const game_preset_model = require('../models/game_preset_model.js');
 const ErrorHandler = require('../modules/error_handler/error_handler.js');
 
+const MIN_EXPRESSION_DIFFICULTY = 1;
+const MAX_EXPRESSION_DIFFICULTY = 3;
+
 class GameService
 {
   getGamePresets(done){
@@ -11,10 +14,68 @@ class GameService
       return done(null, presets);
     });
   }
-  generateAddExpression() {}
-  generateSubtractExpression() {}
-  generateMultiplyExpression() {}
-  generateDivisionExpression() {}
+  generateAddExpression(difficulty){
+    const expr_ceil = this.generateMaxNumber(difficulty);
+    
+    const a = this.getRandomInt(0, expr_ceil);
+    const b = this.getRandomInt(0, expr_ceil);
+
+    const answer = a + b;
+    const expression_str = `${a} + ${b}`;
+
+    return [expression_str, answer];
+  }
+  generateSubtractExpression(difficulty) {
+    const expr_ceil = this.generateMaxNumber(difficulty);
+    
+    const a = this.getRandomInt(0, expr_ceil);
+    const b = this.getRandomInt(0, expr_ceil);
+
+    const answer = a - b;
+    const expression_str = `${a} - ${b}`;
+
+    return [expression_str, answer];
+  }
+  generateMultiplyExpression(difficulty) {
+    const expr_ceil = this.generateMaxNumber(difficulty);
+    
+    const a = this.getRandomInt(0, expr_ceil);
+    const b = this.getRandomInt(0, expr_ceil);
+
+    const answer = a * b;
+    const expression_str = `${a} * ${b}`;
+
+    return [expression_str, answer];
+  }
+  generateDivisionExpression(difficulty) {
+    const expr_ceil = this.generateMaxNumber(difficulty);
+    
+    const a = this.getRandomInt(0, expr_ceil);
+    let b = this.getRandomInt(0, expr_ceil);
+    while(1)
+      if(b == 0) b = this.getRandomInt(0, expr_ceil);
+      else break;
+    
+    const answer = a * b;
+    const expression_str = `${answer} / ${b}`;
+
+    return [expression_str, a];
+  }
+  generateMaxNumber(difficulty) {
+    const diff = Math.max(Math.min(difficulty, MAX_EXPRESSION_DIFFICULTY), MIN_EXPRESSION_DIFFICULTY);   
+    let max = 0;
+ 
+    for(let i = 0; i < diff; i++){
+      max += 9 * Math.pow(10, i);    
+    }
+    return max;
+  }
+  // in range [min, max]
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+  }
 }
 
 
