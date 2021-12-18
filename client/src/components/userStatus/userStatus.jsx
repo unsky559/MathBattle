734c533ed846da847aaa095070916bd8ca1cd096
unsky559 <6kostya@gmail.com>
2021-12-18 20:44:53 +0200

    feat: implement dropdown in userStatus component

--- client/src/components/userStatus/userStatus.jsx
+++ client/src/components/userStatus/userStatus.jsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {createRef, useRef, useState} from 'react';
 import "./userStatus.scss";
+import Dropdown from "../dropdown/dropdown";
+import ButtonList from "../buttonList/buttonList";
+import {useHistory} from "react-router-dom";
 
 const UserStatus = (props) => {
+    const history = useHistory();
+
+    const buttons = [
+        {title: props.userData[0].username,
+        onclick: () => {
+                history.push(`/user/${props.userData[0].username}`);
+            }
+        },
+        {title: "Настройки",
+            onclick: () => {
+                history.push("/settings");
+            }
+        },
+        {title: "Logout",
+            onclick: () => {
+                console.log("do logout");
+                history.push("/login");
+            }
+        }];
     return (
-        <div className="userStatus">
-            <div className="userAvatarName" tabIndex="0">
-                <div className="avatar">
-                    <img src="https://thispersondoesnotexist.com/image" alt=""/>
-                </div>
-                <span className="userName">
-                    {props.userData[0].username}
-                </span>
-            </div>
+        <div className="dropdownHeaderContainer">
+            <Dropdown tabIndex = "0"
+                      header = {
+                        <div className="userStatus">
+                            <div className="userAvatarName">
+                                <div className="avatar">
+                                    <img src="https://thispersondoesnotexist.com/image" alt=""/>
+                                </div>
+
+                                <span className="userName">
+                                    {props.userData[0].username}
+                                </span>
+
+                            </div>
+                        </div>
+            }>
+                <ButtonList
+                    buttons={buttons}/>
+            </Dropdown>
         </div>
     );
 };
