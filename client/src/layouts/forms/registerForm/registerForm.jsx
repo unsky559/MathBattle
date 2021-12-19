574d10500d7a14a6b86f69d361d83a24f80f9dce
unsky559 <6kostya@gmail.com>
2021-12-19 13:45:45 +0200

    feat: add checks to login and register forms

--- client/src/layouts/forms/registerForm/registerForm.jsx
+++ client/src/layouts/forms/registerForm/registerForm.jsx
@@ -4,23 +4,59 @@ import Button from "../../../components/button/button";
 
 import "./registerForm.scss";
 import {apiPostRequest} from "../../../webWorkers/apiRequest";
+import {emailRegex, userNameRegex} from "../../../workers/regex";
 
-const RegisterForm = () => {
+const RegisterForm = (props) => {
 
-    const inputEmail = useState('');
-    const inputUsername = useState('');
-    const inputPassword = useState('');
-    const inputPassword2 = useState('');
+    const inputEmail = useState({val: "", isWrong: false});
+    const inputUsername = useState({val: "", isWrong: false});
+    const inputPassword = useState({val: "", isWrong: false});
+    const inputPassword2 = useState({val: "", isWrong: false});
 
     const submit = () => {
         const data = {
-            "username": inputUsername[0],
-            "email": inputEmail[0],
-            "password": inputPassword[0]
+            username: inputUsername[0].val,
+            email: inputEmail[0].val,
+            password: inputPassword[0].val
         }
 
-        apiPostRequest("reg", data).then((r) => {
-            console.log(r.status)
+        const pass2 = inputPassword2[0].val;
+
+        if(!emailRegex.test(data.email)){
+            inputEmail[1]({...inputEmail[0], isWrong: true});
+            return;
+        }
+
+        if(!userNameRegex.test(data.username)){
+            inputUsername[1]({...inputUsername[0], isWrong: true});
+            return;
+        }
+
+        if(pass2 !== data.password || pass2 === ""){
+            inputPassword2[1]({...inputPassword2[0], isWrong: true});
+            inputPassword[1]({...inputPassword[0], isWrong: true});
+            return;
+        }
+
+        return apiPostRequest("reg", data).then((r) => {
+            if(r.status === 200){
+                props.popupControll(false);
+                props.successPopupController[1](true);
+            }
+            return r.json();
+        }).then((data) => {
+            console.log(data);
+            data.map((dt) => {
+                if(dt.param === "username"){
+                    inputUsername[1]({...inputUsername[0], isWrong: true});
+                }
+                if(dt.param === "password"){
+                    inputPassword[1]({...inputPassword[0], isWrong: true});
+                }
+                if(dt.param === "email"){
+                    inputEmail[1]({...inputEmail[0], isWrong: true});
+                }
+            })
         });
 
     }
