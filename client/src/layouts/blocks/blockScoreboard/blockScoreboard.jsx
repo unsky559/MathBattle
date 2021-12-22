49a44c4ef0e8e496bd611a7027a424928a954506
unsky559 <6kostya@gmail.com>
2021-12-22 02:33:27 +0200

    feat: show player list in scoreboard

--- client/src/layouts/blocks/blockScoreboard/blockScoreboard.jsx
+++ client/src/layouts/blocks/blockScoreboard/blockScoreboard.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import "../../../styles/shared/block.scss";
 import Player from "../../../components/player/player";
 
-const BlockScoreboard = () => {
+const BlockScoreboard = (props) => {
+
+    const scoreboard = props.scoreboard;
+    const data = scoreboard[0];
+
     return (
         <div className="block">
-            <span className="title">Игроки</span>
+            <span className="title">Scoreboard</span>
             <div className="scoreboard">
-                <Player/>
-                <Player/>
+                {data.map((player) => {
+                    console.log(player);
+
+                    if(player.userdata === null){
+                        player.userdata = {
+                            username: "Anonymous"
+                        }
+                    }
+
+                    return <Player player = {player} />
+                })}
             </div>
         </div>
     );
