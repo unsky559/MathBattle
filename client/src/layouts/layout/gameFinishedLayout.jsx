e55b370d09a6b7d1f42f7b628211b7ec84da9a3e
unsky559 <6kostya@gmail.com>
2021-12-22 16:22:27 +0200

    fix: homepage button

--- client/src/layouts/layout/gameFinishedLayout.jsx
+++ client/src/layouts/layout/gameFinishedLayout.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Button from "../../components/button/button";
 import "./gameFinishedLayout.scss";
+import {useHistory} from "react-router-dom";
 
 const GameFinishedLayout = (props) => {
+    const history = useHistory();
     const winText = props.isWin[0] ? "Вы победили" : "Вы проиграли";
+
+    const openHomepage = () => {
+        history.push("/");
+    }
+
     return (
         <div className="gameFinishedLayout">
             <h3 className="hTitle">Игра окончена</h3>
             <p className="p">{winText}</p>
-            <Button text="На главную"/>
+            <Button text="На главную" onClick={openHomepage}/>
         </div>
     );
 };
